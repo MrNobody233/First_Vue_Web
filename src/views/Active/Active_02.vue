8a8103abc77f8c48ce3551029e0ab061cf7b722c
MrNobody233 <1356177686@qq.com>
2022-03-23 21:40:32 +0800

    Merge remote-tracking branch 'origin/master'

--- src/views/Active/Active_02.vue
+++ src/views/Active/Active_02.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="activity">
-    <h3>蓝色技术工作室冬至饺子宴</h3>
+
     <i class="el-icon-date">2021-12-22</i>
     <br>
     <img src="../../assets/Activity/Active02_img_01.png" alt="">
@@ -20,6 +20,55 @@
           <br>
         </p>
       </div>
+      <div class="mountain">
+        <p>冬至的意义</p>
+        <br>
+        <img src="../../assets/Activity/Active02_img_04.png" alt="">
+        <br>
+        <span>
+          冬至又称“冬节”、“贺冬”,
+          <br>
+          华夏二十四节气之一、
+          <br>
+          八大天象类节气之一，
+          <br>
+          与夏至相对。
+          <br>
+          冬至在太阳到达黄经270°时开始，
+          <br>
+          时于每年公历12月22日左右。
+          <br>
+          据传，
+          <br>
+          冬至在历史上的周代是新年元旦，
+          <br>
+          曾经是个很热闹的日子。
+        </span>
+      </div>
+      <!--   why  -->
+      <div class="why">
+        <h2>为什么举办这个活动</h2>
+        <img src="../../assets/Activity/Active02_img_05.png" alt="">
+        <p>冬至，兼具自然与人文两大内涵，既是二十四节气中一个重要的节气，也是中国民间的传统节日，素有“冬至大如年”之说。
+          <br>
+          这个学期已经走到了末尾，大家伙平日除了学习培训，就是在忙碌于准备明年的比赛，如此努力的大家，怎能不犒劳一下，大家一起放松放松？</p>
+        <div class="why_img">
+          <img src="../../assets/Activity/Active02_img_06.png" alt="">
+          <img src="../../assets/Activity/Active02_img_07.png" alt="">
+        </div>
+      </div>
+      <div class="eat">
+        <h2>成果展示</h2>
+        <img src="../../assets/Activity/Active02_img_05.png" alt="" style="width: 60px">
+
+        <div class="eat_img">
+          <img src="../../assets/Activity/Active02_img_08.png" alt="">
+          <br>
+          <img src="../../assets/Activity/Active02_img_09.png" alt="">
+          <img src="../../assets/Activity/Active02_img_10.png" alt="">
+        </div>
+      </div>
+
     </div>
   </div>
 </template>
@@ -52,7 +101,8 @@ export default {
 
     p {
       font-weight: bold;
-      letter-spacing: 3px
+      letter-spacing: 3px;
+      font-family: 'Microsoft YaHei ! important;';
     }
 
     .top_img {
@@ -60,23 +110,77 @@ export default {
       height: 205px;
       background: url("../../assets/Activity/Active02_img_02.png") no-repeat;
       background-size: 100%;
-      h1{
+
+      h1 {
         padding-top: 100px;
       }
-      p{
+
+      p {
         font-weight: normal;
         font-size: 14px;
       }
     }
-    .bottom_img{
+
+    .bottom_img {
       width: 100%;
       height: 200px;
       background: url("../../assets/Activity/Active02_img_03.png") no-repeat;
       background-size: 100%;
 
-      p{
+      p {
         font-weight: normal;
         font-size: 12px;
+        line-height: 25px;
+      }
+    }
+
+    .mountain {
+      p {
+        font-size: 19px;
+      }
+
+      span {
+        font-size: 12px;
+        line-height: 20px;
+      }
+    }
+
+    .why {
+      margin: 40px auto 0 auto;
+
+      h2 {
+        font-size: 19px;
+      }
+
+      img {
+        width: 50px;
+      }
+
+      p {
+        font-size: 12px;
+        font-weight: normal;
+        margin: 0 30px 0px 30px;
+        line-height: 20px;
+      }
+
+      .why_img {
+
+        margin: auto;
+
+        img {
+          width: 100px;
+          padding-left: 20px;
+
+        }
+      }
+    }
+
+    .eat {
+      margin: 40px auto 0 auto;
+
+      img {
+        width: 200px;
+
       }
     }
   }
--- src/views/Active/Active_03.vue
+++ src/views/Active/Active_03.vue
@@ -1,4 +1,121 @@
 <template>
+  <div class="active">
+    <span class="mafan">
+      年度总结
+    </span>
+    <van-divider :style="{ color: '#1989fa', borderColor: '#1989fa', padding: '0 16px' }">
+      <img src="../../assets/avatar.png" alt="" width="50px">
+    </van-divider>
+    <p>
+      新年的到来
+      <br>
+      是对过往的检讨
+      <br>
+      对未来的期望
+      <br>
+      2021已去
+      <br>
+      对过去的一年
+      <br>
+      总要有句结束语
+      <br>
+      跟着零碎的图片
+      <br>
+      共同回忆惆怅的2021
+    </p>
+
+    <div class="content_box">
+      <img src="../../assets/Activity/gif_01.gif" width="50px">
+      <br><br>
+      <span>全国大学生工程训练综合能力大赛</span>
+      <br><br>
+      <img src="../../assets/Activity/Active03_img_01.png" alt="" width="300px" height="200px">
+      <p>
+        全国大学生工程训练综合能力竞赛是教育部高等教育司发文，教育部、财政部共同资助的大学生竞赛项目，属于学校A类学科竞赛。竞赛每两年一届，实行校、省（直辖市、自治区）全国三级竞赛制度，以校级竞赛为基础，逐级选拔进入上一级比赛。在此次比赛中，工作室参与竞赛并荣获佳绩。
+      </p>
+    </div>
+
+    <div class="content_box">
+      <img src="../../assets/Activity/gif_02.gif" width="50px">
+      <br><br>
+      <span>全国大学生智能汽车竞速赛</span>
+      <br><br>
+      <img src="../../assets/Activity/Active03_img_02.png" alt="" width="300px" height="300px">
+      <p>
+        关于这项比赛：
+        <br>
+        全国大学生“飞思卡尔”杯智能汽车竞赛起源于韩国，是韩国汉阳大学汽车控制实验室在飞思卡尔半导体公司资助下举办的以hcs12单片机为核心的大学生课外科技竞赛。组委会提供一个标准的汽车模型、直流电机和可充电式电池，参赛队伍要制作一个能够自主识别路径的智能车，在专门设计的跑道上自动识别道路行驶，最快跑完全程而没有冲出跑道并且技术报告评分较高为获胜者。其设计内容涵盖了控制、模式识别、传感技术、汽车电子、电气、计算机、机械、能源等多个学科的知识，对学生的知识融合和实践动手能力的培养，具有良好的推动作用。
+        <br>
+        蓝色技术工作室自此参加该项比赛已有八年，荣获多项省级及国家级奖项。
+      </p>
+    </div>
+
+    <div class="content_box">
+      <img src="../../assets/Activity/gif_03.gif" width="50px">
+      <br><br>
+
+      <span>全国三维数字化创新设计大赛</span>
+      <br><br>
+      <img src="../../assets/Activity/Active03_img_03.png" alt="" width="300px" height="300px">
+      <p>
+        关于这项比赛：
+        <br>
+        全国三维数字化创新设计大赛（简称“全国 3D 大赛”或
+        3DDS），是在国家大力实施创新驱动发展战略、推动实体经济和数字经济融合发展的时代背景下开展的一项大型公益赛事，以“三维数字化”与“创新设计”为特色，突出体现科技进步和产业升级的要求，是大众创新、万众创业的具体实践。
+        <br>
+        工作室今年在此项比赛中锐意进取，披荆斩棘，终是有两支队伍挺进国赛，取得了优秀的成绩。
+      </p>
+    </div>
+
+    <div class="content_box">
+      <img src="../../assets/Activity/gif_04.gif" width="50px">
+      <br><br>
+
+      <span>算法队与软件队所获成果展示</span>
+      <br><br>
+      <img src="../../assets/Activity/Active03_img_04.png" alt="" width="300px" height="300px">
+      <p>
+        部分比赛介绍：
+        <br>
+        蓝桥杯全国软件和信息技术专业人才大赛是由工业和信息化部人才交流中心举办的全国性IT学科赛事。共有北京大学、清华大学、上海交通大学等全国1200余所高校参赛，累计参赛人数超过40万人。
+        <br>
+        ACM国际大学生程序设计竞赛是由国际计算机协会（ACM）主办的，一项旨在展示大学生创新能力、团队精神和在压力下编写程序、分析和解决问题能力的年度竞赛。经过近40年的发展，ACM国际大学生程序设计竞赛已经发展成为全球最具影响力的大学生程序设计竞赛，赛事由AWS、华为和Jetbrains赞助，在北京大学设有ICPC北京总部用于组织东亚区域赛。
+      </p>
+    </div>
+
+    <div class="content_box">
+      <img src="../../assets/Activity/gif_05.gif" width="50px">
+      <br><br>
+
+      <span>培训现场</span>
+      <br><br>
+      <img src="../../assets/Activity/Active03_img_05.png" alt="" width="350px" height="300px">
+      <img src="../../assets/Activity/Active03_img_06.png" alt="" width="350px" height="300px">
+      <img src="../../assets/Activity/Active03_img_07.png" alt="" width="350px" height="400px">
+      <p>
+        为新进会员，进行老生带新生的技术培训，是蓝色技术工作室长久以来的传统，老生甘当枝叶，帮助新生快速成长，使工作室的未来力量，逐渐成长壮大。
+      </p>
+    </div>
+
+    <hr style="color: blue;">
+    <span style="line-height: 30px;">
+      新的一年
+<br>
+谨代表蓝色技术工作室祝大家
+<br>
+元旦快乐新年快乐
+<br>
+也祝愿蓝色技术工作室的大家
+<br>
+2022年里
+<br>
+排除万难勇攀高峰
+<br>
+取得更优秀的成绩
+    </span>
+    <br>
+    <img src="../../assets/Activity/Active03_img_08.png" alt="" width="150px">
+  </div>
 
 </template>
 
@@ -8,6 +125,38 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="less">
+.active {
+  max-width: 450px;
+  margin: 46px auto 0 auto;
+  text-align: center;
+  background: url("../../assets/Activity/background01.png");
+  background-size: 100%;
 
+  .mafan {
+    font-size: 36px;
+    visibility: visible;
+    text-shadow: rgb(0, 0, 0) 1px 1px 0px, rgb(0, 0, 0) 1px -1px 0px, rgb(0, 0, 0) -1px 1px 0px, rgb(0, 0, 0) -1px -1px 0px, rgb(0, 0, 0) 0px 1.4px 0px, rgb(0, 0, 0) 0px -1.4px 0px, rgb(0, 0, 0) -1.4px 0px 0px, rgb(0, 0, 0) 1.4px 0px 0px, rgb(141, 179, 226) 0px 3px 0px, rgb(0, 0, 0) 1px 4px 0px, rgb(0, 0, 0) 1px 2px 0px, rgb(0, 0, 0) -1px 4px 0px, rgb(0, 0, 0) -1px 2px 0px, rgb(0, 0, 0) 0px 4.4px 0px, rgb(0, 0, 0) 0px 1.6px 0px, rgb(0, 0, 0) -1.4px 3px 0px, rgb(0, 0, 0) 1.4px 3px 0px;
+    color: white;
+  }
+
+  p {
+    color: #00b0f0;
+    line-height: 30px;
+  }
+
+  .content_box {
+    margin-bottom: 10px;
+
+    p {
+      margin: 0 30px 0 30px;
+      font-size: 14px;
+    }
+
+    span {
+      font-weight: bold;
+      font-size: 16px;
+    }
+  }
+}
 </style>
